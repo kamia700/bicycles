439fb8f122e208c41ee7562dab9ce23a6b04b4d4
kamia700 <kaana757@gmail.com>
2020-10-10 09:40:12 +0200

    add img

--- gulpfile.js
+++ gulpfile.js
@@ -68,7 +68,7 @@ gulp.task("images", function () {
 gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
   .pipe(webp({quality:90}))
-  .pipe(gulp.dest("build/img"));
+  .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("sprite", function () {
