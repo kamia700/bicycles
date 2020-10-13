297560bf92e74921a62d387b00f6ae2c69f7b5a5
kamia700 <kaana757@gmail.com>
2020-10-13 23:32:43 +0200

    facts3

---
+++ build/js/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+(function () {
+  var nav = document.querySelector('.page-header__block');
+  var toggle = document.querySelector('.page-header__toggle');
+  var body = document.querySelector('body');
+  // var header = document.querySelector('.page-header');
+
+  // nav.classList.remove('page-header--nojs');
+  body.classList.remove('nojs');
+  nav.classList.add('page-header--close');
+
+  toggle.addEventListener('click', function () {
+    if (nav.classList.contains('page-header--open')) {
+      nav.classList.remove('page-header--open');
+      nav.classList.add('page-header--close');
+      body.classList.remove('lock');
+    } else {
+      nav.classList.remove('page-header--close');
+      nav.classList.add('page-header--open');
+      body.classList.add('lock');
+    }
+  });
+
+}());
